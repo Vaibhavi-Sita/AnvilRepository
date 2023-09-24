062a59aa1478b27afc3f224792d7edc3463cf3fa
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2023-09-24 16:34:44 +0530

    configuration

--- CompetitiveProgrammingPracticeApp/src/main/java/com/vsita/cp/model/Topic.java
+++ CompetitiveProgrammingPracticeApp/src/main/java/com/vsita/cp/model/Topic.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.springframework.stereotype.Component;
 
-@Entity
+//@Entity
+@Component
 public class Topic {
 
     @Id
--- CompetitiveProgrammingPracticeApp/src/test/java/com/vsita/cp/CpApplicationTests.java
+++ CompetitiveProgrammingPracticeApp/src/test/java/com/vsita/cp/CpApplicationTests.java
@@ -2,8 +2,11 @@ package com.vsita.cp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(locations = "/test-context.xml")
+
 class CpApplicationTests {
 
 	@Test
