94b55339059bc9ddb6aeeecf0c72aaa08697d969
Vaibhavi-Sita <52885102+Vaibhavi-Sita@users.noreply.github.com>
2023-09-25 01:02:23 +0530

    update component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-function Greeting(){
-    return <h2>My First Component</h2>;
+function Greeting() {
+  return <h2>Hello World</h2>
+}
+
+const Person = () => <h2>sita</h2>
+const Message = () => {
+  return <p>this is a message</p>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Greeting/>);
-
-// export default Greeting;
+root.render(<Greeting />)
